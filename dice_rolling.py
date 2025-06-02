96c98a3a55cbc9e55bdcd331007ca7ff876228e4
Kavyareddy1622 <167638592+Kavyareddy1622@users.noreply.github.com>
2025-06-02 18:50:46 +0530

    Add files via upload

---
+++ dice_rolling.py
@@ -0,0 +1,18 @@
+import random
+
+def dice_roll():
+    
+    while True:
+        choice = input("Roll the dice? (y/n): ").lower()
+        if choice == 'y':
+            die1 = random.randint(1, 6)
+            die2 = random.randint(1, 6)
+            print(f"({die1}, {die2})")
+        elif choice == 'n':
+            print("Thank you for playing!")
+            break
+        else:
+            print("Invalid input")
+if __name__ == "__main__":
+    dice_roll()
+    
